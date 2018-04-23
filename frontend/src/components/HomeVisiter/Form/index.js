9cf486d53d371f2ed40d7cf454fdae1da8e90d03
Aymeric Guillien <aymericguillien@gmail.com>
2018-04-23 16:57:56 +0200

    Page home visiteur inté React OK

---
+++ frontend/src/components/HomeVisiter/Form/index.js
@@ -0,0 +1,25 @@
+/**
+ * Npm import
+ */
+import React from 'react';
+/**
+* Local import
+*/
+
+/**
+ * Code
+ */
+const Form = () => (
+  <form className="" action="index.html" method="post">
+    <label for="login">Login</label>
+    <input id="login" type="text" name="login" value="" />
+    <label for="password">Mot de passe</label>
+    <input id="password" type="password" name="password" value="" />
+    <a href="#" id="forgotPassword">Mot de passe perdu?</a>
+    {/* <button id="buttonSubmit" type="button" name="button">Envoyer</button> */}
+  </form>
+);
+/**
+ * Export
+ */
+export default Form;
