6644c5239e03bfb0b209f8cfb340b5ca4ee7df9a
jimmy.rekwian <rekwian@gmx.fr>
2018-04-25 17:50:47 +0200

    add getAllGames

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -2,12 +2,25 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Game;
+use JMS\Serializer\SerializerBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class GameController extends Controller
 {
 	public function listAction()
 	{
 		return $this->render('game/list.html.twig');
+	}
+
+	public function getAllGames()
+	{
+		$games = $this->getDoctrine()->getRepository(Game::class)->findAll();
+
+		$serializer = SerializerBuilder::create()->build();
+		$jsonContent = $serializer->serialize($games, 'json');
+
+		return new Response($jsonContent);
 	}
 }
