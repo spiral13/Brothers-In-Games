e3c535e5cd60491df9d36497c77e5e26193f1ab4
jimmy.rekwian <rekwian@gmx.fr>
2018-04-25 16:31:16 +0200

    add jms/serializer

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -3,15 +3,19 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Article;
+use JMS\Serializer\SerializerBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends Controller
 {
-	public function getHomeArticleAction(SerializerInterface $serializer)
+	public function getHomeArticleAction()
 	{
 		$news = $this->getDoctrine()->getRepository(Article::class)->findSoMuchFormTheLast();
 
-		return $this->json(['data' => $news]);
+		$serializer = SerializerBuilder::create()->build();
+		$jsonContent = $serializer->serialize($news, 'json');
+
+		return new Response($jsonContent);
 	}
 }
