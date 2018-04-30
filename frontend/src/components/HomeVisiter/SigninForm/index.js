62f8c9f0c5d651f7c13599369fa31721cbaf302f
Aymeric Guillien <aymericguillien@gmail.com>
2018-04-30 12:37:41 +0200

    erge

--- frontend/src/components/HomeVisiter/SigninForm/index.js
+++ frontend/src/components/HomeVisiter/SigninForm/index.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import ArrowRight from 'react-icons/lib/fa/arrow-right';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 /**
 * Local import
 */
@@ -12,13 +13,45 @@ import PropTypes from 'prop-types';
  * Code
  */
 class SigninForm extends React.Component {
+  state = {
+    _username: {
+      agreed: false,
+      error: false,
+    },
+    _password: {
+      agreed: false,
+      error: false,
+    },
+  }
+
   submitForm = (evt) => {
     evt.preventDefault();
-    this.props.actions.submitConnect();
+    if (this.props.username !== '' && this.props.password !== '') {
+      this.props.actions.submitConnect();
+    }
+    // else if (this.props.username !== '' && this.props.password === '') {
+    //
+    // }
+  }
+
+  changeInput = ({ target }) => {
+    if (target.value === '') {
+      this.setState({
+        [target.name]: {
+          ...this.state[target.name],
+          agreed: true,
+          error: true,
+        },
+      });
+    }
+    // else {
+    //   this.setState({ [target.name]: 'agreed' });
+    // }
+    this.props.actions.changeForm(target.name, target.value);
   }
 
   render() {
-    const { username, password, actions } = this.props;
+    const { username, password } = this.props;
     return (
       <form
         method="post"
@@ -26,18 +59,20 @@ class SigninForm extends React.Component {
       >
         <label htmlFor="username">Login</label>
         <input
+          className={classNames({ error: this.state._username.error })}
           id="username"
           type="text"
-          name="username"
-          onChange={({ target }) => actions.changeLoginForm(target.value)}
+          name="_username"
+          onChange={this.changeInput}
           value={username}
         />
         <label htmlFor="password">Mot de passe</label>
         <input
+          className={classNames({ error: this.state._password.error })}
           id="password"
           type="password"
-          name="password"
-          onChange={({ target }) => actions.changePasswordForm(target.value)}
+          name="_password"
+          onChange={this.changeInput}
           value={password}
         />
         <a href="#" id="forgotPassword">Mot de passe perdu?</a>
