81179261057b46b4984104351b9e20c0c90d3c97
Benjamin Tournier <bftournier@gmail.com>
2018-05-02 09:08:20 +0200

    merge

--- frontend/src/components/MyGames/Main/index.js
+++ frontend/src/components/MyGames/Main/index.js
@@ -12,7 +12,34 @@ import React from 'react';
  */
 const Main = () => (
   <div id="ContainerMyGamesList">
-    My Games List
+
+    <div id="infoIdentityProfile">
+      <img id="photoProfileOnMyGames" src="https://media.koreus.com/201701/allez-faire-loutre.jpg" alt="Toto" />
+      <h1 id="profileNameOnMyGames">Liste de jeux de Toto</h1>
+    </div>
+
+    <section id="sectionListingMyGames">
+      <div id="settings">
+        <h2 id="title">Vos jeux</h2>
+        <div id="triggerLegend">Ajouter un nouveau jeu à votre liste</div>
+        <span id="trigger"> + </span>
+      </div>
+      <div id="triggeredAddOneGameForm">
+        <form id="addOneGameForm">
+          <input
+            type="text"
+            placeholder="Ajouter un nouveau jeu à votre liste"
+          />
+        </form>
+      </div>
+
+      <div id="containerAllMyGames">
+        <div id="oneOfMyGame">
+          <img src="https://media.koreus.com/201701/allez-faire-loutre.jpg" alt="titre du jeu" id="oneOfMyGamePicture" />
+          <h2 id="oneOfMyGameTitle">titre du jeu</h2>
+        </div>
+      </div>
+    </section>
   </div>
 );
 // Main.propTypes = {
--- frontend/src/components/MyGames/index.js
+++ frontend/src/components/MyGames/index.js
@@ -8,7 +8,7 @@ import React from 'react';
 */
 import Sidebar from 'frontend/src/containers/Navigation_sidebar/Sidebar';
 import Navbar from 'frontend/src/containers/Navigation_sidebar/Navbar';
-// import Main from 'frontend/src/components/MyGames/Main';
+import Main from 'frontend/src/components/MyGames/Main';
 import Loading from 'frontend/src/components/Loading';
 
 /**
@@ -43,34 +43,7 @@ class MyGames extends React.Component {
       <div className="MyGamesList">
         <Navbar />
         <Sidebar />
-        <div id="ContainerMyGamesList">
-
-          <div id="infoIdentityProfile">
-            <img id="photoProfileOnMyGames" src="https://media.koreus.com/201701/allez-faire-loutre.jpg" alt="Toto" />
-            <h1 id="profileNameOnMyGames">Liste de jeux de Toto</h1>
-          </div>
-
-          <section id="sectionListingMyGames">
-            <div id="settings">
-              <h2 id="title">Vos jeux</h2>
-              <div id="triggerLegend">Ajouter un nouveau jeu à votre liste</div>
-              <span id="trigger"> + </span>
-            </div>
-            <div id="triggeredAddOneGameForm">
-              <form id="addOneGameForm">
-                <input
-                  type="text"
-                  placeholder="Ajouter un nouveau jeu à votre liste"
-                />
-              </form>
-            </div>
-
-            <div id="MyGames">
-              <img src="https://media.koreus.com/201701/allez-faire-loutre.jpg" alt="titre du jeu" id="oneOfMyGamePicture" />
-              <h2 id="oneOfMyGameTitle">titre du jeu</h2>
-            </div>
-          </section>
-        </div>
+        <Main />
       </div>
     );
   }
