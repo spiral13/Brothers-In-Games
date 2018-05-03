a1c92e6e2393d2cce152952d18ee8d9d6fab9c36
Aymeric Guillien <aymericguillien@gmail.com>
2018-05-03 12:18:43 +0200

    merge

--- frontend/src/components/Navigation_sidebar/Sidebar/index.js
+++ frontend/src/components/Navigation_sidebar/Sidebar/index.js
@@ -7,15 +7,29 @@ import PropTypes from 'prop-types';
 * Local import
 */
 import Friends from 'frontend/src/components/Navigation_sidebar/Friends';
+import Loading from 'frontend/src/components/Loading';
 /**
  * Code
  */
 class Sidebar extends React.Component {
+  state = {
+    loading: true,
+  }
+
+  componentWillMount() {
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 500);
+  }
+
   redirection = (value) => {
     this.props.actions.redirect(value);
   }
 
   render() {
+    if (this.state.loading) {
+      return <Loading />;
+    }
     return (
       <div id="sidebar">
         <h1 id="sidebar-title">
