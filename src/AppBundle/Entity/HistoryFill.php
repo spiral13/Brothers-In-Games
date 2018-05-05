ac9447941ab41d5a5ce8abf986b6ea24a1f27adf
Rekwian <rekwian@gmx.fr>
2018-05-05 18:54:18 +0200

    Patch Command 'fill with API', Add fields for historyFill
    split to two command, one for fill "gameCategories" table, other one for fill "game" table with the previous included.
    change loop process

--- src/AppBundle/Entity/HistoryFill.php
+++ src/AppBundle/Entity/HistoryFill.php
@@ -13,6 +13,41 @@ class HistoryFill
     private $id;
 
     /**
+     * @var int
+     */
+    private $maxData;
+
+    /**
+     * @var int
+     */
+    private $remainingData;
+
+    /**
+     * @var int
+     */
+    private $processedData;
+
+    /**
+     * @var int
+     */
+    private $maxPage;
+
+    /**
+     * @var int
+     */
+    private $remainingPage;
+
+    /**
+     * @var int
+     */
+    private $processedPage;
+
+    /**
+     * @var int
+     */
+    private $dataPerPage;
+
+    /**
      * @var string
      */
     private $name;
@@ -21,16 +56,6 @@ class HistoryFill
      * @var string
      */
     private $url;
-
-    /**
-     * @var int
-     */
-    private $maxData;
-
-    /**
-     * @var int
-     */
-    private $page;
 
     /**
      * @var \DateTime
@@ -51,6 +76,170 @@ class HistoryFill
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set maxData
+     *
+     * @param integer $maxData
+     *
+     * @return HistoryFill
+     */
+    public function setMaxData($maxData)
+    {
+        $this->maxData = $maxData;
+
+        return $this;
+    }
+
+    /**
+     * Get maxData
+     *
+     * @return int
+     */
+    public function getMaxData()
+    {
+        return $this->maxData;
+    }
+
+    /**
+     * Set remainingData
+     *
+     * @param integer $remainingData
+     *
+     * @return HistoryFill
+     */
+    public function setRemainingData($remainingData)
+    {
+        $this->remainingData = $remainingData;
+
+        return $this;
+    }
+
+    /**
+     * Get remainingData
+     *
+     * @return int
+     */
+    public function getRemainingData()
+    {
+        return $this->remainingData;
+    }
+
+    /**
+     * Set processedData
+     *
+     * @param integer $processedData
+     *
+     * @return HistoryFill
+     */
+    public function setProcessedData($processedData)
+    {
+        $this->processedData = $processedData;
+
+        return $this;
+    }
+
+    /**
+     * Get processedData
+     *
+     * @return int
+     */
+    public function getProcessedData()
+    {
+        return $this->processedData;
+    }
+
+    /**
+     * Set maxPage
+     *
+     * @param integer $maxPage
+     *
+     * @return HistoryFill
+     */
+    public function setMaxPage($maxPage)
+    {
+        $this->maxPage = $maxPage;
+
+        return $this;
+    }
+
+    /**
+     * Get maxPage
+     *
+     * @return int
+     */
+    public function getMaxPage()
+    {
+        return $this->maxPage;
+    }
+
+    /**
+     * Set remainingPage
+     *
+     * @param integer $remainingPage
+     *
+     * @return HistoryFill
+     */
+    public function setRemainingPage($remainingPage)
+    {
+        $this->remainingPage = $remainingPage;
+
+        return $this;
+    }
+
+    /**
+     * Get remainingPage
+     *
+     * @return int
+     */
+    public function getRemainingPage()
+    {
+        return $this->remainingPage;
+    }
+
+    /**
+     * Set processedPage
+     *
+     * @param integer $processedPage
+     *
+     * @return HistoryFill
+     */
+    public function setProcessedPage($processedPage)
+    {
+        $this->processedPage = $processedPage;
+
+        return $this;
+    }
+
+    /**
+     * Get processedPage
+     *
+     * @return int
+     */
+    public function getProcessedPage()
+    {
+        return $this->processedPage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDataPerPage()
+    {
+        return $this->dataPerPage;
+    }
+
+    /**
+     * @param int $dataPerPage
+     *
+     * @return self
+     */
+    public function setDataPerPage($dataPerPage)
+    {
+        $this->dataPerPage = $dataPerPage;
+
+        return $this;
     }
 
     /**
@@ -99,54 +288,6 @@ class HistoryFill
     public function getUrl()
     {
         return $this->url;
-    }
-
-    /**
-     * Set maxData
-     *
-     * @param integer $maxData
-     *
-     * @return HistoryFill
-     */
-    public function setMaxData($maxData)
-    {
-        $this->maxData = $maxData;
-
-        return $this;
-    }
-
-    /**
-     * Get maxData
-     *
-     * @return int
-     */
-    public function getMaxData()
-    {
-        return $this->maxData;
-    }
-
-    /**
-     * Set page
-     *
-     * @param integer $page
-     *
-     * @return HistoryFill
-     */
-    public function setPage($page)
-    {
-        $this->page = $page;
-
-        return $this;
-    }
-
-    /**
-     * Get page
-     *
-     * @return int
-     */
-    public function getPage()
-    {
-        return $this->page;
     }
 
     /**
