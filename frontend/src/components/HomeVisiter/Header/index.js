1e426880652703fd21da8591ea99d666219dba26
Rekwian <rekwian@gmx.fr>
2018-05-06 18:46:10 +0200

    Updated css home visitor completed

--- frontend/src/components/HomeVisiter/Header/index.js
+++ frontend/src/components/HomeVisiter/Header/index.js
@@ -18,32 +18,34 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <div id="leftSide">
-          <h1>
-            Brothers
-            <br />
-            <span><i>in</i>Games</span>
-          </h1>
-          <p className="login_signin">Se connecter</p>
-          <SigninForm />
-          {/* eslint-disable-next-line */}
-          <button
-            className="signup"
-            onClick={() => this.setState({ isClicked: !this.state.isClicked })}
-          >
-            S'inscrire
-          </button>
-          {this.state.isClicked &&
-          <div>
-            <SignupForm />
-          </div>}
-        </div>
+            {this.state.isClicked &&
+            <div>
+              <SignupForm />
+            </div>}
+        <div id="angle">
+          <div id="leftSide">
+            <h1>
+              Brothers
+              <br />
+              <span><i>in</i>Games</span>
+            </h1>
+            <p className="login_signin">Se connecter</p>
+            <SigninForm />
+            {/* eslint-disable-next-line */}
+            <button
+              className="signup"
+              onClick={() => this.setState({ isClicked: !this.state.isClicked })}
+            >
+              S'inscrire
+            </button>
+          </div>
 
-        <div id="tom_clancy" />
-        <div className="underplace">
-          <h2>Lorem Ipsum</h2>
-          {/* eslint-disable-next-line */}
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <div id="tom_clancy" />
+          <div className="underplace">
+            <h2>Lorem Ipsum</h2>
+            {/* eslint-disable-next-line */}
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
         </div>
       </header>
     );
