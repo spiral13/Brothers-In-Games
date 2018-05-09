e54213ee1db8781f955dd50753a18bdb1bddb0bc
Benjamin Tournier <bftournier@gmail.com>
2018-05-09 22:54:31 +0200

    MyProfile: toggle items with public or not information for settings pop-up

--- frontend/src/components/MyProfile/Main/index.js
+++ frontend/src/components/MyProfile/Main/index.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import FaPencil from 'react-icons/lib/fa/pencil';
 import FaEllipsisH from 'react-icons/lib/fa/ellipsis-h';
 import Select from 'react-select';
+import MdCancel from 'react-icons/lib/md/cancel';
 
 /**
 * Local import
@@ -20,7 +21,7 @@ import MyProfileSettings from 'frontend/src/components/MyProfile/MyProfileSettin
  */
 class Main extends React.Component {
   state = {
-    settingsForMyProfilemIsClicked: false,
+    settingsForMyProfileIsClicked: false,
     selectedOption: '',
   }
 
@@ -37,7 +38,7 @@ class Main extends React.Component {
   render() {
     const { selectedOption } = this.state;
     const { myFriend } = this.props.userInformation[0];
-    const { user } = this.props.userInformation;
+    const user = this.props.userInformation;
     let allOptions = [];
     myFriend.map((option) => {
       allOptions = [...allOptions, { value: option.username, label: option.username, id: option.id }];
@@ -51,19 +52,29 @@ class Main extends React.Component {
         </section>
         <a
           id="settingsForMyProfileTrigger"
-          onClick={() => this.setState({ settingsForMyProfilemIsClicked: !this.state.settingsForMyProfilemIsClicked })}
+          onClick={() => this.setState({ settingsForMyProfileIsClicked: !this.state.settingsForMyProfileIsClicked })}
         > <FaEllipsisH /> <FaPencil />
         </a>
 
-        {this.state.settingsForMyProfilemIsClicked &&
-        <MyProfileSettings />
-        }
+        {this.state.settingsForMyProfileIsClicked &&
+        <div id="triggeredSettingsForm">
+          <MdCancel
+            onClick={() =>
+              this.setState({ settingsForMyProfileIsClicked: !this.state.settingsForMyProfileIsClicked })}
+            id="crossCancelDeleteForm"
+          />
+          <div id="changeYourInformation">Modifiez vos informations</div>
+          <MyProfileSettings />
+        </div>}
 
-        <MyProfileBasicsInformation />
+        <MyProfileBasicsInformation
+          user={user}
+        />
 
         <section id="AddOrDeleteFriendFromMyProfile">
           <form id="AddFriendFromMyProfile" className="FriendsFromMyProfile">
-            {/* Ici mettre le nom du joueur à ajouter dans value puis avec le onChange envoyer le nom du joueur dans le reducer : FriendReducer */}
+            {/* Ici mettre le nom du joueur à ajouter dans value puis
+              avec le onChange envoyer le nom du joueur dans le reducer : FriendReducer */}
             {/* <input value={} onChange={} /> */}
             Test
           </form>
--- frontend/src/components/MyProfile/MyProfileSettings/index.js
+++ frontend/src/components/MyProfile/MyProfileSettings/index.js
@@ -3,6 +3,10 @@
  */
 import React from 'react';
 // import PropTypes from 'prop-types';
+import FaToggleOff from 'react-icons/lib/fa/toggle-off';
+import FaToggleOn from 'react-icons/lib/fa/toggle-on';
+// import ImageUploader from 'react-image-upload';
+
 
 /**
 * Local import
@@ -11,32 +15,106 @@ import React from 'react';
 /**
  * Code
  */
-const MyProfileSettings = () => (
-  <section id="settingsForMyProfile">
-    <div id="changeYourInformation">Modifiez vos informations</div>
-    <div id="photoUploader">  x Téléchargez votre photo de profil </div>
-    <input
-      id="inputFirstName"
-      placeholder="Prénom"
-    />
+class MyProfileSettings extends React.Component {
+  state = {
+    firstNameIsPublic: false,
+    familyNameIsPublic: false,
+    nickNameIsPublic: false,
+    ageIsPublic: false,
+  }
+  render() {
+    return (
+      <section id="settingsForMyProfile">
+        {/* <div >  x Téléchargez votre photo de profil </div> */}
 
-    <input
-      id="inputFamilyName"
-      placeholder="Nom"
-    />
+        <section id="settingsInputs">
+          {/* <ImageUploader
+            buttonText="Choose images"
+            onChange={this.onDrop}
+            id="photoUploader"
+            imgExtension={['.jpg', '.gif', '.png', '.gif']}
+            maxFileSize={5242880}
+          /> */}
+          <input
+            id="inputFirstName"
+            placeholder="Prénom"
+          />
+          {this.state.firstNameIsPublic &&
+            <FaToggleOn
+              className="publicToggle-on"
+              onClick={() =>
+                this.setState({ firstNameIsPublic: !this.state.firstNameIsPublic })}
+            />
+          }
+          {!this.state.firstNameIsPublic &&
+          <FaToggleOff
+            className="publicToggle-off"
+            onClick={() =>
+              this.setState({ firstNameIsPublic: !this.state.firstNameIsPublic })}
+          />
+          }
+          <input
+            id="inputFamilyName"
+            placeholder="Nom"
+          />
+          {this.state.familyNameIsPublic &&
+            <FaToggleOn
+              className="publicToggle-on"
+              onClick={() =>
+                this.setState({ familyNameIsPublic: !this.state.familyNameIsPublic })}
+            />
+          }
+          {!this.state.familyNameIsPublic &&
+          <FaToggleOff
+            className="publicToggle-off"
+            onClick={() =>
+              this.setState({ familyNameIsPublic: !this.state.familyNameIsPublic })}
+          />
+          }
 
-    <input
-      id="inputNickName"
-      placeholder="Surnom de ton avatar"
-    />
+          <input
+            id="inputNickName"
+            placeholder="Surnom de ton avatar"
+          />
+          {this.state.nickNameIsPublic &&
+            <FaToggleOn
+              className="publicToggle-on"
+              onClick={() =>
+                this.setState({ nickNameIsPublic: !this.state.nickNameIsPublic })}
+            />
+          }
+          {!this.state.nickNameIsPublic &&
+          <FaToggleOff
+            className="publicToggle-off"
+            onClick={() =>
+              this.setState({ nickNameIsPublic: !this.state.nickNameIsPublic })}
+          />
+          }
 
-    <input
-      id="inputAge"
-      placeholder="âge"
-    />
+          <input
+            id="inputAge"
+            placeholder="âge"
+          />
+          {this.state.ageIsPublic &&
+            <FaToggleOn
+              className="publicToggle-on"
+              onClick={() =>
+                this.setState({ ageIsPublic: !this.state.ageIsPublic })}
+            />
+          }
+          {!this.state.ageIsPublic &&
+          <FaToggleOff
+            className="publicToggle-off"
+            onClick={() =>
+              this.setState({ ageIsPublic: !this.state.ageIsPublic })}
+          />
+          }
 
-  </section>
-);
+        </section>
+      </section>
+    );
+  }
+}
 
 // Game.propTypes = {
 //   game: PropTypes.object.isRequired,
