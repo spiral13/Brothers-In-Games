e7406b9d3fd5158a76667dcee40723ed603a1171
Benjamin Tournier <bftournier@gmail.com>
2018-05-15 10:43:27 +0200

    final look for contact Page

---
+++ frontend/src/components/Contact/Main/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Main = () => (
+  <div id="contactUsContainer">
+    <h1 className="titleH1Information">Nous contacter</h1>
+    <div className="sectionContactInformation">
+      <h2 className="titleH2ContactInformation">Adresse</h2>
+      <p className="contentMandatoryInformation">Top of the World après le 3e cumulonimbus – 0000 Heaven</p>
+    </div>
+
+    <div className="sectionContactInformation">
+      <h2 className="titleH2ContactInformation">E-Mailing</h2>
+      <p className="contentMandatoryInformation">contact[at]brothersingames.fr</p>
+    </div>
+
+
+  </div>
+);
+
+export default Main;
