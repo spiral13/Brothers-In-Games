1f4327bb4890bfc8c3064a3f951ee4f8a01ad083
Benjamin Tournier <bftournier@gmail.com>
2018-05-15 10:51:52 +0200

    coorection css contact page

--- frontend/src/components/Contact/Main/index.js
+++ frontend/src/components/Contact/Main/index.js
@@ -6,12 +6,12 @@ const Main = () => (
     <h1 className="titleH1Information">Nous contacter</h1>
     <div className="sectionContactInformation">
       <h2 className="titleH2ContactInformation">Adresse</h2>
-      <p className="contentMandatoryInformation">Top of the World après le 3e cumulonimbus – 0000 Heaven</p>
+      <p className="contentMandatoryContactInformation">Top of the World après le 3e cumulonimbus – 0000 Heaven</p>
     </div>
 
     <div className="sectionContactInformation">
       <h2 className="titleH2ContactInformation">E-Mailing</h2>
-      <p className="contentMandatoryInformation">contact[at]brothersingames.fr</p>
+      <p className="contentMandatoryContactInformation">contact[at]brothersingames.fr</p>
     </div>
 
 
